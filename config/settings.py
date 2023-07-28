be5251cdb38a4cf4d1865f7fbe75c4625654c1ee
berdibaevich <berdibaevichjetker@gmail.com>
2023-07-28 20:47:50 +0500

    (v1) default database

--- config/settings.py
+++ config/settings.py
@@ -97,18 +97,18 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
-# # RENDER DATABASE SERVER 
 # DATABASES = {
-#     'default': dj_database_url.parse(env("DATABASE_URL"))
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
 # }
-# # END RENDER DATABASE SERVER
+
+# RENDER DATABASE SERVER 
+DATABASES = {
+    'default': dj_database_url.parse(env("DATABASE_URL"))
+}
+# END RENDER DATABASE SERVER
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
